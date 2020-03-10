38ca4fddfff070deaf57b77c25b434d7167cf576
Cory Tanner <ctannerweb@gmail.com>
2020-03-10 16:03:16 -0400

    Add and remove TODO tasks (#1)
    * Add and remove TODO tasks
    
    * Quick Cleanup

---
+++ src/copmonents/addTask.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react'
+import Task from './task'
+
+class AddTask extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todoTask: [],
+      message: '',
+    }
+    
+    this.handleClickChange = this.handletodoTask.bind(this)
+    this.removeTask = this.removeTask.bind(this)
+  }
+
+  handletodoTask(e) {
+    e.preventDefault();
+    const {todoTask} = this.state;
+    const newTask = this.newTask.value;
+
+    const isOnTheList = todoTask.includes(newTask);
+    if(isOnTheList) {
+      this.setState({
+        message: 'This Task is already on the list'
+      })
+
+    } else {
+      newTask !== '' && this.setState({
+        todoTask: [...todoTask, newTask],
+        message: ''
+      });
+    }
+
+    this.addForm.reset();
+  }
+  removeTask(task) {
+    const newtodoTask = this.state.todoTask.filter(todoTask => {
+      return todoTask !== task;
+    })
+
+    this.setState({
+      todoTask: [...newtodoTask]
+    })
+  }
+
+  render () {
+    const {todoTask, message} = this.state;
+
+    return (
+      <>
+      <form ref={input => this.addForm = input} onSubmit={(e) => {this.handletodoTask(e)}}>
+        <label htmlFor="newTask">Add New Task</label>
+        <input id="newTask" ref={input => this.newTask = input} type="text"/>
+
+        <button type="submit" id="newTask">Add Task</button>
+      </form>
+
+      {
+        message !== '' && <p>{message}</p>
+      }
+
+      <ul>
+        {
+          todoTask.map(todoTask => {
+            return (
+              <>
+              <Task onClick={this.removeTask} taskCopy={todoTask}/>
+              <button onClick={(e) => this.removeTask(todoTask)} className="task__remove-task">Remove Task</button>
+              </>
+            )
+          })
+        }
+      </ul>
+      </>
+    )
+  }
+}
+
+export default AddTask;
