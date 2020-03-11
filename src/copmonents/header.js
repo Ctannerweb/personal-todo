0dfaae381d069e810c8a31d9017bfc1ea8bd7197
Cory Tanner <ctannerweb@gmail.com>
2020-03-11 09:07:58 -0600

    Fixes #2 - Style TODO Task flow

---
+++ src/copmonents/header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => (
+  <header className="header">
+    <h1 className="t-display header__display">TODO Task Tracker</h1>
+  </header>
+)
+
+export default Header;
