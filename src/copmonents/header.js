4f471e8e4dd38a253316032b347730d3b6460beb
Cory Tanner <ctannerweb@gmail.com>
2020-03-14 21:01:43 -0600

    Add Favicon and fixes #4 - add task complete functionality (#5)

--- src/copmonents/header.js
+++ src/copmonents/header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => (
   <header className="header">
-    <h1 className="t-display header__display">TODO Task Tracker</h1>
+    <h1 className="t-display header__display">Personal TODO</h1>
   </header>
 )
 
--- src/copmonents/task.js
+++ src/copmonents/task.js
@@ -1,17 +1,35 @@
 import React, { Component } from 'react'
 
 class Task extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      taskCompleted: false
+    }
+
+    this.toggleTaskCompleted = this.toggleTaskCompleted.bind(this);
+  }
+
+  toggleTaskCompleted = () => {
+    this.setState((currentState) => ({
+      taskCompleted: !currentState.taskCompleted
+    }))
+  }
+
   render () {
-    const {taskCopy, triggerRemoveTask} = this.props;
+    const {taskCompleted} = this.state;
+    const {taskCopy, triggerRemoveTask, taskID} = this.props;
+
     return (
-      <li className="task">
-        <label className="task__checkbox" htmlFor="taskComplete">
-          <span className="t-hidden">Mark Task complete</span>
-          <input id="taskComplete" type="checkbox" />
+      <li className="task" data-state-completed={taskCompleted ? "true" : "false"}>
+        <label className="task__checkbox" htmlFor={taskID}>
+          <span className="t-hidden">Mark TODO complete</span>
+          <input onClick={this.toggleTaskCompleted} id={taskID} type="checkbox" />
           <span className="task__checkbox__custom"></span>
         </label>
         <p className="task__copy t-body">{taskCopy}</p>
-        <button onClick={triggerRemoveTask} className="task__remove-task t-button">Remove Task</button>
+        <button onClick={triggerRemoveTask} className="task__remove-task t-button">Remove TODO</button>
       </li>
     )
   }
