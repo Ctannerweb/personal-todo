a012a1e5cc8f6bca28034e2fe2781253a3502ba0
Cory Tanner <ctannerweb@gmail.com>
2020-07-26 11:31:53 -0600

    Add google A

--- src/App.js
+++ src/App.js
@@ -3,8 +3,15 @@ import './App.scss';
 import Main from './copmonents/main'
 import Header from './copmonents/header';
 import Footer from './copmonents/footer';
+import ReactGA from 'react-ga';
+
+function initializeAnalytics() {
+  ReactGA.initialize('UA-154481477-3');
+  ReactGA.pageview('/index');
+}
 
 function App() {
+  initializeAnalytics()
   return (
     <div className="App">
       <Header />
