e119808d83845f7ffafd44eca20c61bffd8d49f7
Cory Tanner <ctannerweb@gmail.com>
2020-07-26 12:06:29 -0600

    Add support copy

--- src/copmonents/main.js
+++ src/copmonents/main.js
@@ -7,6 +7,10 @@ class Main extends Component {
     return (
       <main className="main">
         <AddTask />
+        <section className="section-support">
+          <h3 className="section__support-heading t-body-heading">Was this useful?</h3>
+          <p className="section__support-copy t-support-copy">Support the developer by using this <a href="https://brave.com/cor272" className="t-link">Brave Referral Link</a> and give the web browser a test run!</p>
+        </section>
       </main>
     )
   }
